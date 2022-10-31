56d4c9c509203669f158a32afe1364ae1ce29808
andrey.bh <andrey.bh@gmail.com>
2022-10-31 13:14:02 +0200

    Implementing test module

---
+++ src/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Rapa Test Module
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Rapa_Test',
+    __DIR__
+);
